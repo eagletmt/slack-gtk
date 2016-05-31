a4143bc7cb6ab833da5c64729ccde02e42e3d3dd
Kohei Suzuki <eagletmt@gmail.com>
2016-06-01 02:20:05 +0900

    Change message entry to TextView

--- include/message_entry.h
+++ include/message_entry.h
@@ -1,18 +1,19 @@
 #ifndef SLACK_GTK_MESSAGE_ENTRY_H
 #define SLACK_GTK_MESSAGE_ENTRY_H
 
-#include <gtkmm/entry.h>
+#include <gtkmm/textview.h>
 #include "api_client.h"
 
-class MessageEntry : public Gtk::Entry {
+class MessageEntry : public Gtk::TextView {
  public:
   MessageEntry(const api_client& api_client, const std::string& channel_id);
   virtual ~MessageEntry();
 
  protected:
-  virtual void on_activate() override;
+  virtual bool on_key_press_event(GdkEventKey* event) override;
 
  private:
+  bool on_enter();
   void post_message_finished(const boost::optional<Json::Value>& result) const;
 
   api_client api_client_;
--- src/message_entry.cc
+++ src/message_entry.cc
@@ -4,14 +4,27 @@
 MessageEntry::MessageEntry(const api_client& api_client,
                            const std::string& channel_id)
     : api_client_(api_client), channel_id_(channel_id) {
+  set_wrap_mode(Gtk::WRAP_WORD);
+  // TODO: Styling
+  set_border_width(5);
 }
 
 MessageEntry::~MessageEntry() {
 }
 
-void MessageEntry::on_activate() {
-  const Glib::ustring text = get_text();
-  set_text("");
+bool MessageEntry::on_key_press_event(GdkEventKey* event) {
+  switch (event->keyval) {
+    case GDK_KEY_Return:
+    case GDK_KEY_KP_Enter:
+      return on_enter();
+  }
+  return Gtk::TextView::on_key_press_event(event);
+}
+
+bool MessageEntry::on_enter() {
+  Glib::RefPtr<Gtk::TextBuffer> buffer = get_buffer();
+  const Glib::ustring text = buffer->get_text();
+  buffer->set_text("");
   std::map<std::string, std::string> params;
   params["channel"] = channel_id_;
   params["text"] = text.raw();
@@ -20,6 +33,7 @@ void MessageEntry::on_activate() {
   api_client_.queue_post("chat.postMessage", params,
                          std::bind(&MessageEntry::post_message_finished, this,
                                    std::placeholders::_1));
+  return true;
 }
 
 void MessageEntry::post_message_finished(
