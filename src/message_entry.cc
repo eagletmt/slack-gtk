664a896eea403aad2fcd956b9fb81e00c3240958
Kohei Suzuki <eagletmt@gmail.com>
2016-06-03 01:09:00 +0900

    Add style to message entry

--- src/message_entry.cc
+++ src/message_entry.cc
@@ -1,12 +1,16 @@
 #include "message_entry.h"
+#include <gtkmm/cssprovider.h>
+#include <gtkmm/stylecontext.h>
 #include <iostream>
 
 MessageEntry::MessageEntry(const api_client& api_client,
                            const std::string& channel_id)
     : api_client_(api_client), channel_id_(channel_id) {
   set_wrap_mode(Gtk::WRAP_WORD);
-  // TODO: Styling
-  set_border_width(5);
+  Glib::RefPtr<Gtk::CssProvider> provider = Gtk::CssProvider::create();
+  provider->load_from_resource("/cc/wanko/slack-gtk/message_entry.css");
+  get_style_context()->add_provider(provider,
+                                    GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 }
 
 MessageEntry::~MessageEntry() {
